5b1f39640c3acc2776ec30e53eeaa2fa134b4a8d
git-sshd-76d5549466-gmg99-root <4981046@gmail.com>
2026-02-20 10:01:17 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,8 @@ fi
 #chrome remote desktop display
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=$CODE --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$HOSTNAME --pin=$PIN
 HOST_HASH=$(echo -n "$(hostname)" | md5sum | awk '{print $1}')  
-FILENAME=.config/chrome-remote-desktop/host#${HOST_HASH}.json 
-mv .config/chrome-remote-desktop/host#*.json $FILENAME  
+#FILENAME=.config/chrome-remote-desktop/host#${HOST_HASH}.json 
+#mv .config/chrome-remote-desktop/host#*.json $FILENAME  
 sudo service chrome-remote-desktop stop 
 sudo service chrome-remote-desktop start 
 
